f2be55f72a2ac2c19e4bc0eda2c4c2c5d75d109a
Adetola Adedeji <adetolaadedeji0@gmail.com>
2025-06-09 04:55:06 +0100

    Update mojo create state

--- programs/invariant/src/instructions/create_state.rs
+++ programs/invariant/src/instructions/create_state.rs
@@ -36,16 +36,23 @@ use anchor_lang::solana_program::system_program;
 #[derive(Accounts)]
 #[instruction(nonce: u8)]
 pub struct CreateState<'info> {
-    #[account(init, seeds = [b"statev1".as_ref()], bump, payer = admin, space = 8 + std::mem::size_of::<State>())]
+    #[account(
+        init,
+        seeds = [b"state"],
+        bump,
+        payer = admin,
+        space = 8 + std::mem::size_of::<State>()
+    )]
     pub state: AccountLoader<'info, State>,
 
     #[account(mut)]
     pub admin: Signer<'info>,
 
-    pub base_token_mint: AccountInfo<'info>,
-    pub fee_collector: AccountInfo<'info>,
-
-    #[account(seeds = [b"Invariant".as_ref()], bump = nonce)]
+    /// CHECK: This is a program-derived-address used in downstream vault/mint control.
+    #[account(
+        seeds = [b"mojo"],
+        bump = nonce
+    )]
     pub program_authority: AccountInfo<'info>,
 
     pub rent: Sysvar<'info, Rent>,
@@ -54,20 +61,16 @@ pub struct CreateState<'info> {
     pub system_program: AccountInfo<'info>,
 }
 
-pub fn handler(ctx: Context<CreateState>, nonce: u8) -> ProgramResult {
-    msg!("MOJO: INITIALIZING GLOBAL STATE");
+pub fn handler(ctx: Context<CreateState>, nonce: u8) -> Result<()> {
+    msg!("MOJO-AMM: Initializing Global State");
 
     let state = &mut ctx.accounts.state.load_init()?;
 
-    **state = State {
+    *state = State {
         admin: *ctx.accounts.admin.key,
         authority: *ctx.accounts.program_authority.key,
-        base_token_mint: *ctx.accounts.base_token_mint.key,
-        fee_collector: *ctx.accounts.fee_collector.key,
         nonce,
         bump: *ctx.bumps.get("state").unwrap(),
-        is_paused: false,
-        fee_tier_count: 0,
     };
 
     Ok(())
--- programs/invariant/src/structs/state.rs
+++ programs/invariant/src/structs/state.rs
@@ -1,27 +1,27 @@
-// use anchor_lang::prelude::*;
-
-// #[account(zero_copy)]
-// #[repr(packed)]
-// #[derive(PartialEq, Default, Debug)]
-// pub struct State {
-//     pub admin: Pubkey,
-//     pub nonce: u8,
-//     pub authority: Pubkey,
-//     pub bump: u8,
-// }
-
 use anchor_lang::prelude::*;
 
 #[account(zero_copy)]
 #[repr(packed)]
 #[derive(PartialEq, Default, Debug)]
 pub struct State {
-    pub admin: Pubkey,           // Owner authority
-    pub authority: Pubkey,       // PDA program authority
-    pub base_token_mint: Pubkey, // Always MOJO
-    pub fee_collector: Pubkey,   // Treasury destination
+    pub admin: Pubkey,
     pub nonce: u8,
+    pub authority: Pubkey,
     pub bump: u8,
-    pub is_paused: bool,     // Emergency circuit breaker
-    pub fee_tier_count: u16, // Tracks fee tier configs (optional extension)
 }
+
+// use anchor_lang::prelude::*;
+
+// #[account(zero_copy)]
+// #[repr(packed)]
+// #[derive(PartialEq, Default, Debug)]
+// pub struct State {
+//     pub admin: Pubkey,           // Owner authority
+//     pub authority: Pubkey,       // PDA program authority
+//     pub base_token_mint: Pubkey, // Always MOJO
+//     pub fee_collector: Pubkey,   // Treasury destination
+//     pub nonce: u8,
+//     pub bump: u8,
+//     pub is_paused: bool,     // Emergency circuit breaker
+//     pub fee_tier_count: u16, // Tracks fee tier configs (optional extension)
+// }
