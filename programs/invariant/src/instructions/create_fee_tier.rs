fcc2cb5f661b4113a8917011dbd497f521067be7
Adetola Adedeji <adetolaadedeji0@gmail.com>
2025-06-09 05:03:13 +0100

    Update create fee tier

--- programs/invariant/src/instructions/create_fee_tier.rs
+++ programs/invariant/src/instructions/create_fee_tier.rs
@@ -1,36 +1,49 @@
 use crate::decimals::*;
 use crate::structs::fee_tier::FeeTier;
+use crate::structs::state::State;
 use crate::ErrorCode::*;
-use crate::*;
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::system_program;
 
 #[derive(Accounts)]
 #[instruction(fee: u128, tick_spacing: u16)]
 pub struct CreateFeeTier<'info> {
-    #[account(init,
-        seeds = [b"feetierv1", program_id.as_ref(), &fee.to_le_bytes(), &tick_spacing.to_le_bytes()],
-        bump, payer = admin
+    #[account(
+        init,
+        seeds = [b"feetier", program_id.as_ref(), &fee.to_le_bytes(), &tick_spacing.to_le_bytes()],
+        bump,
+        payer = admin,
+        space = 8 + std::mem::size_of::<FeeTier>() // ensure enough space is allocated
     )]
     pub fee_tier: AccountLoader<'info, FeeTier>,
-    #[account(seeds = [b"statev1".as_ref()], bump = state.load()?.bump)]
+
+    #[account(
+        seeds = [b"state".as_ref()],
+        bump = state.load()?.bump
+    )]
     pub state: AccountLoader<'info, State>,
-    #[account(mut, constraint = &state.load()?.admin == admin.key @ InvalidAdmin)]
+
+    #[account(
+        mut,
+        constraint = state.load()?.admin == admin.key() @ InvalidAdmin
+    )]
     pub admin: Signer<'info>,
+
     pub rent: Sysvar<'info, Rent>,
+
     #[account(address = system_program::ID)]
     pub system_program: AccountInfo<'info>,
 }
 
 impl<'info> CreateFeeTier<'info> {
     pub fn handler(&self, fee: u128, tick_spacing: u16, bump: u8) -> ProgramResult {
-        msg!("INVARIANT: CREATE FEE TIER");
+        msg!("MOJO: CREATE FEE TIER");
 
         require!(tick_spacing > 0, InvalidTickSpacing);
-        let fee_tier = &mut self.fee_tier.load_init()?;
+        let mut fee_tier = self.fee_tier.load_init()?;
         let fee = FixedPoint::new(fee);
 
-        **fee_tier = FeeTier {
+        *fee_tier = FeeTier {
             fee,
             tick_spacing,
             bump,
--- programs/invariant/src/structs/fee_tier.rs
+++ programs/invariant/src/structs/fee_tier.rs
@@ -5,7 +5,7 @@ use anchor_lang::prelude::*;
 #[repr(packed)]
 #[derive(PartialEq, Default, Debug)]
 pub struct FeeTier {
-    pub fee: FixedPoint,
-    pub tick_spacing: u16,
+    pub fee: FixedPoint,   // e.g., 3000 = 0.3%
+    pub tick_spacing: u16, // required for range AMMs
     pub bump: u8,
 }
